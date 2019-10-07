570024d82432d0dc724840782302961b8b97c690
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-07 21:33:21 +0200

    Extend the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
-import 'package:hunger_preventer/data/models/app_state.dart';
+import 'package:hunger_preventer/data/app_state.dart';
+import 'package:hunger_preventer/data/transaction_repository.dart';
 import 'package:hunger_preventer/screens/home/home.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -10,9 +11,11 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
+  var transactionRepository = TransactionRepository();
+  var appState = AppState(transactionRepository);
   runApp(
     ScopedModel<AppState>(
-      model: AppState(),
+      model: appState,
       child: CupertinoApp(
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
