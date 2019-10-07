be10f22fa4ac7681a88d7d25210583474ee6233d
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-07 19:13:35 +0200

    Refactore app state package name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
-import 'package:hunger_preventer/data/app_state.dart';
+import 'package:hunger_preventer/data/models/app_state.dart';
 import 'package:hunger_preventer/screens/home/home.dart';
 import 'package:scoped_model/scoped_model.dart';
 
