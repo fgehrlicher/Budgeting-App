b30b6a28e24ca49ce7a179e364052edefc01e540
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-08 08:54:53 +0200

    Add custom list for transactions

--- lib/data/models/transaction_list.dart
+++ lib/data/models/transaction_list.dart
@@ -1,5 +1,8 @@
 import 'package:hunger_preventer/data/models/transaction.dart';
+import 'package:quiver/collection.dart';
 
-class TransactionList {
-  List<Transaction> _transactions;
+class TransactionList extends DelegatingList<Transaction> {
+  final List<Transaction> _transactions = [];
+
+  List<Transaction> get delegate => _transactions;
 }
