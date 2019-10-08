0e8f600e584ee82d3ff67f17c21363f504257e7a
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-08 23:00:06 +0200

    Adjust sorting of the dummy data

--- lib/data/transaction_repository.dart
+++ lib/data/transaction_repository.dart
@@ -6,10 +6,10 @@ class TransactionRepository {
     until ??= DateTime.now();
     var list = TransactionList();
 
-    list.add(Transaction(DateTime.parse("2012-02-27 12:00:00"), 2800));
-    list.add(Transaction(DateTime.parse("2022-02-27 12:00:00"), -30.00));
-    list.add(Transaction(DateTime.parse("2019-02-27 12:00:00"), -20.00));
-    list.add(Transaction(DateTime.parse("2018-02-27 12:00:00"), -100.23));
+    list.add(Transaction(DateTime.parse("2012-01-07 13:00:00"), 2800));
+    list.add(Transaction(DateTime.parse("2022-08-12 16:00:00"), -30.00));
+    list.add(Transaction(DateTime.parse("2019-01-06 20:00:00"), -20.00));
+    list.add(Transaction(DateTime.parse("2018-12-28 09:00:00"), -100.23));
 
     return list;
   }
