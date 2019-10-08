de39083388f6c3f5018f03effabb0162a024fc7a
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-08 22:59:21 +0200

    Add getter for the app state transaction repo

--- lib/data/app_state.dart
+++ lib/data/app_state.dart
@@ -2,8 +2,10 @@ import 'package:hunger_preventer/data/transaction_repository.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class AppState extends Model {
-
   TransactionRepository _transactionRepository;
 
   AppState(this._transactionRepository);
+
+  TransactionRepository get transactionRepository => _transactionRepository;
 }
+t
