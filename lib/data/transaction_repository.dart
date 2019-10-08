13d15a119af8595a99657f627f5d43d32b4b8914
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-08 09:15:56 +0200

    Add dummy get method to the transaction repository

--- lib/data/transaction_repository.dart
+++ lib/data/transaction_repository.dart
@@ -1,3 +1,17 @@
-class TransactionRepository {
+import 'package:hunger_preventer/data/models/transaction.dart';
+import 'package:hunger_preventer/data/models/transaction_list.dart';
 
+class TransactionRepository {
+  TransactionList get(DateTime from, [DateTime until]) {
+    until ??= DateTime.now();
+    var list = TransactionList();
+
+    list.add(Transaction(DateTime.now(), 2800));
+    list.add(Transaction(DateTime.now(), -100.23));
+    list.add(Transaction(DateTime.now(), -20.00));
+    list.add(Transaction(DateTime.now(), -30.00));
+
+    return list;
+  }
 }
+ 
