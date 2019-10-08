d7a10e6630c19a856d74e0fb5973dbc5fe0c15d8
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-08 22:21:34 +0200

    Add dummy data to the transaction repository

--- lib/data/transaction_repository.dart
+++ lib/data/transaction_repository.dart
@@ -6,10 +6,10 @@ class TransactionRepository {
     until ??= DateTime.now();
     var list = TransactionList();
 
-    list.add(Transaction(DateTime.now(), 2800));
-    list.add(Transaction(DateTime.now(), -100.23));
-    list.add(Transaction(DateTime.now(), -20.00));
-    list.add(Transaction(DateTime.now(), -30.00));
+    list.add(Transaction(DateTime.parse("2012-02-27 12:00:00"), 2800));
+    list.add(Transaction(DateTime.parse("2022-02-27 12:00:00"), -30.00));
+    list.add(Transaction(DateTime.parse("2019-02-27 12:00:00"), -20.00));
+    list.add(Transaction(DateTime.parse("2018-02-27 12:00:00"), -100.23));
 
     return list;
   }
