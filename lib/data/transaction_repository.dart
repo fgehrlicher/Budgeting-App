b6fb2f509459131f7b3a9a2e849abe0de144bfff
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-10 09:15:41 +0200

    Refactore transaction repository

--- lib/data/transaction_repository.dart
+++ lib/data/transaction_repository.dart
@@ -6,12 +6,12 @@ class TransactionRepository {
     until ??= DateTime.now();
     var list = TransactionList();
 
-    list.add(Transaction(DateTime.parse("2012-01-07 13:00:00"), 2800));
-    list.add(Transaction(DateTime.parse("2022-08-12 16:00:00"), -30.00));
-    list.add(Transaction(DateTime.parse("2019-01-06 20:00:00"), -20.00));
-    list.add(Transaction(DateTime.parse("2018-12-28 09:00:00"), -100.23));
+    list
+      ..add(Transaction(DateTime.parse("2012-01-07 13:00:00"), 2800))
+      ..add(Transaction(DateTime.parse("2022-08-12 16:00:00"), -30.00))
+      ..add(Transaction(DateTime.parse("2019-01-06 20:00:00"), -20.00))
+      ..add(Transaction(DateTime.parse("2018-12-28 09:00:00"), -100.23));
 
     return list;
   }
 }
- 
