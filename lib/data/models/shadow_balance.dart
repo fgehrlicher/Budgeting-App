5de8e152e58187e43d185abf84c24584d74b1ba0
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-13 22:00:33 +0200

    Add the shadow balance model

---
+++ lib/data/models/shadow_balance.dart
@@ -0,0 +1,5 @@
+import 'package:hunger_preventer/data/models/balance_snapshot.dart';
+
+class ShadowBalance extends BalanceSnapshot {
+  ShadowBalance(double balance, DateTime date) : super(balance, date);
+}
