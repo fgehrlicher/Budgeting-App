a9d3701dfbc84468f08c3b6c8f4ea21bd49d82d4
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-17 22:09:34 +0200

    Remove repostitory mock data

--- lib/data/repositories/balance_snapshot_repository.dart
+++ lib/data/repositories/balance_snapshot_repository.dart
@@ -3,11 +3,6 @@ import 'package:hunger_preventer/data/models/balance_snapshot.dart';
 class BalanceSnapshotRepository {
   List<AccountBalanceSnapshot> get(DateTime from, [DateTime until]) {
     until ??= DateTime.now();
-    var balanceSnapshots = [
-      AccountBalanceSnapshot(0, DateTime.parse("2020-01-01 00:00:00")),
-      AccountBalanceSnapshot(2700, DateTime.parse("2020-01-02 13:00:00")),
-      AccountBalanceSnapshot(10000, DateTime.parse("2020-01-10 13:00:00"))
-    ];
-    return balanceSnapshots;
+    return null;
   }
 }
--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -1,23 +1,9 @@
-import 'package:hunger_preventer/data/models/transaction.dart';
 import 'package:hunger_preventer/data/models/transaction_list.dart';
 
 class TransactionRepository {
   TransactionList get(DateTime from, [DateTime until]) {
     until ??= DateTime.now();
     var list = TransactionList();
-
-    list
-      ..add(Transaction(DateTime.parse("2020-01-01 13:00:00"), 2800))
-      ..add(Transaction(DateTime.parse("2020-01-02 16:00:00"), -30.00))
-      ..add(Transaction(DateTime.parse("2020-01-03 20:00:00"), -20.00))
-      ..add(Transaction(DateTime.parse("2020-01-05 09:00:00"), -100.23))
-      ..add(Transaction(DateTime.parse("2020-01-06 09:00:00"), 2800))
-      ..add(Transaction(DateTime.parse("2020-01-07 09:00:00"), -100.23))
-      ..add(Transaction(DateTime.parse("2020-01-08 09:00:00"), -100.23))
-      ..add(Transaction(DateTime.parse("2020-01-09 09:00:00"), -100.23))
-      ..add(Transaction(DateTime.parse("2020-01-12 09:00:00"), 500))
-      ..add(Transaction(DateTime.parse("2020-01-13 09:00:00"), -145));
-
     return list;
   }
 }
