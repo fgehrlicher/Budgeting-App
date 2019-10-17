b0d9147389cced2f781782031b519fc084c5d0c3
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-17 21:05:03 +0200

    Extend the transaction model

--- lib/data/models/transaction.dart
+++ lib/data/models/transaction.dart
@@ -1,11 +1,19 @@
+import 'package:hunger_preventer/data/models/transaction_type.dart';
+
 class Transaction {
   final DateTime _date;
   final double _amount;
+  final TransactionType _type;
+  final String _iban;
+  final String _bic;
 
   Transaction(
     this._date,
-    this._amount,
-  );
+    this._amount, [
+    this._type,
+    this._iban,
+    this._bic,
+  ]);
 
   double get amount => _amount;
 
