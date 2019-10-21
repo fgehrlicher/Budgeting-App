99ac073d045b7c98fbe42db54e25a301fccd88af
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-21 23:01:08 +0200

    Refactore app state

--- lib/data/app_state.dart
+++ lib/data/app_state.dart
@@ -2,9 +2,5 @@ import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class AppState extends Model {
-  TransactionRepository _transactionRepository;
-
-  AppState(this._transactionRepository);
-
-  TransactionRepository get transactionRepository => _transactionRepository;
+  AppState();
 }
