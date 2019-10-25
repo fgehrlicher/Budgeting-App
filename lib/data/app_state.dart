da6d8525d9c62284ea1e1c1e82e6f1044606c202
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-25 22:15:22 +0200

    Remove app state

--- lib/data/app_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
-import 'package:scoped_model/scoped_model.dart';
-
-class AppState extends Model {
-  AppState();
-}
