94f40da4c4cf440820f4acc9d26169318eba61ac
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-27 19:38:25 +0100

    Remove unused import

--- lib/presentation/screens/transaction_list/transaction_list.dart
+++ lib/presentation/screens/transaction_list/transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hunger_preventer/screens/transaction_list/transaction_list_state.dart';
 
 class TransactionList extends StatefulWidget {
   @override
