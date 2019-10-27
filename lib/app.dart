540bfa43a6fc9f4ad790802260305671310e0276
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-27 18:39:38 +0100

    Refactore main app file

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
-import 'package:hunger_preventer/data/app_state.dart';
-import 'package:hunger_preventer/data/database/database_provider.dart';
-import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
-import 'package:hunger_preventer/screens/home/home.dart';
-import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([
@@ -12,14 +7,10 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  var appState = AppState();
   runApp(
-    ScopedModel<AppState>(
-      model: appState,
-      child: CupertinoApp(
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
     ),
   );
 }
