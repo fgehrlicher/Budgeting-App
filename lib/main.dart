cd820dcdcfbe7f273d90d0fdee4f2ac2febbcad3
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-28 08:21:23 +0100

    Add fix for the "defaultBinaryMessenger was accessed before the binding was initialized" exeception

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:hunger_preventer/presentation/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
