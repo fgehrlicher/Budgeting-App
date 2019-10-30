98eb6112800f232d931bf0c4ef39cafb98b42d91
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-30 13:36:50 +0100

    Add inital transaction list bloc state

--- lib/domain/bloc/transaction_list/transaction_list_bloc.dart
+++ lib/domain/bloc/transaction_list/transaction_list_bloc.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_event.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_state.dart';
 
 
 class TransactionListBloc extends Bloc<TransactionListEvent, TransactionListState> {
+  final TransactionRepository _transactionRepository;
+
+  TransactionListBloc(this._transactionRepository);
+
   @override
-  TransactionListState get initialState => InitialTransactionListState();
+  TransactionListState get initialState => TransactionsEmpty();
 
   @override
   Stream<TransactionListState> mapEventToState(
