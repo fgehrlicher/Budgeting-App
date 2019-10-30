f2428736d85ac52b468ca70ee4273fca245ddc8f
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-30 11:42:21 +0100

    Add transaction list bloc states

--- lib/domain/bloc/transaction_list/state.dart
+++ lib/domain/bloc/transaction_list/state.dart
@@ -1,10 +1,23 @@
 import 'package:equatable/equatable.dart';
+import 'package:hunger_preventer/domain/models/transaction.dart';
 
 abstract class TransactionListState extends Equatable {
   const TransactionListState();
-}
 
-class InitialTransactionListState extends TransactionListState {
   @override
   List<Object> get props => [];
 }
+
+class TransactionsEmpty extends TransactionListState{}
+
+class TransactionsLoading extends TransactionListState{}
+
+class TransactionsLoaded extends TransactionListState{
+  final List<Transaction> transactions;
+
+  const TransactionsLoaded(this.transactions);
+
+  @override
+  List<Object> get props => [transactions];
+}
+
