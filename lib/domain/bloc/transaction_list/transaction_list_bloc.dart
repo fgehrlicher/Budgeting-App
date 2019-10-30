1ff839dc74b455e0541e91c51130eba153634d2c
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-30 14:55:05 +0100

    Implement initial transaction list mapEventToState

--- lib/domain/bloc/transaction_list/transaction_list_bloc.dart
+++ lib/domain/bloc/transaction_list/transaction_list_bloc.dart
@@ -4,8 +4,8 @@ import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_event.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_state.dart';
 
-
-class TransactionListBloc extends Bloc<TransactionListEvent, TransactionListState> {
+class TransactionListBloc
+    extends Bloc<TransactionListEvent, TransactionListState> {
   final TransactionRepository _transactionRepository;
 
   TransactionListBloc(this._transactionRepository);
@@ -17,6 +17,14 @@ class TransactionListBloc extends Bloc<TransactionListEvent, TransactionListStat
   Stream<TransactionListState> mapEventToState(
     TransactionListEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is FetchTransactions) {
+      yield TransactionsLoading();
+      var transactions = await this._transactionRepository.getAll();
+      if (transactions.length > 0) {
+        yield TransactionsLoaded(transactions);
+      } else {
+        yield TransactionsEmpty();
+      }
+    }
   }
 }
