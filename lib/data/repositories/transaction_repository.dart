838efad54dde0f0ad7edbffc33d1f44fbf38eb03
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-31 16:14:56 +0100

    Refactore the transaction repository to use the static db provider

--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -1,38 +1,41 @@
+import 'package:hunger_preventer/data/database/database_provider.dart';
 import 'package:hunger_preventer/domain/models/transaction.dart' as model;
-import 'package:hunger_preventer/data/repositories/repository.dart';
 import 'package:hunger_preventer/domain/models/transaction_list.dart';
 import 'package:sqflite/sqflite.dart';
 
-class TransactionRepository extends Repository {
-  TransactionRepository(Database db) : super(db);
+class TransactionRepository {
+  TransactionRepository(Database db);
 
   Future<TransactionList> get(DateTime from, [DateTime until]) async {
     until ??= DateTime.now();
     var list = TransactionList();
+    var database = await DatabaseProvider.database;
 
-    final List<Map<String, dynamic>> maps =
-        await this.database.query('transaction');
+    final List<Map<String, dynamic>> maps = await database.query('transaction');
 
-    list.addAll(List.generate(maps.length, (i) {
-      return model.Transaction(
-        DateTime.fromMillisecondsSinceEpoch(maps[i]['date'] * 1000),
-          maps[i]['amount']
-      );
-    }));
+    list.addAll(
+      List.generate(maps.length, (i) {
+        return model.Transaction(
+            DateTime.fromMillisecondsSinceEpoch(maps[i]['date'] * 1000),
+            maps[i]['amount']);
+      }),
+    );
     return list;
   }
 
   Future<TransactionList> getAll() async {
     var list = TransactionList();
-    final List<Map<String, dynamic>> maps =
-        await this.database.query('transaction');
+    var database = await DatabaseProvider.database;
 
-    list.addAll(List.generate(maps.length, (i) {
-      return model.Transaction(
-        DateTime.fromMillisecondsSinceEpoch(maps[i]['date'] * 1000),
-          maps[i]['amount']
-      );
-    }));
+    final List<Map<String, dynamic>> maps = await database.query('transaction');
+
+    list.addAll(
+      List.generate(maps.length, (i) {
+        return model.Transaction(
+            DateTime.fromMillisecondsSinceEpoch(maps[i]['date'] * 1000),
+            maps[i]['amount']);
+      }),
+    );
     return list;
   }
 }
