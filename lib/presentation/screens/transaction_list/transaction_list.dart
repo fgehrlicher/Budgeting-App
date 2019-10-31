0754a4d8ab1acc26b55d6b945eef0424d45fc87e
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-10-31 18:27:08 +0100

    Add switch for the different transaction states

--- lib/presentation/screens/transaction_list/transaction_list.dart
+++ lib/presentation/screens/transaction_list/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_bloc.dart';
 import 'package:hunger_preventer/domain/bloc/transaction_list/transaction_list_state.dart';
@@ -9,7 +10,6 @@ class TransactionList extends StatefulWidget {
 }
 
 class _TransactionListState extends State<TransactionList> {
-
   TransactionListBloc _transactionListBloc;
 
   @override
@@ -21,7 +21,24 @@ class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TransactionListBloc, TransactionListState>(
-    builder: (context, state) {}
+      builder: (context, state) {
+        if (state is TransactionsLoaded) {
+          return Center(
+            child: Text('transactions found'),
+          );
+        }
+        if (state is TransactionsEmpty) {
+          return Center(
+            child: Text('no transactions'),
+          );
+        }
+        if (state is TransactionsLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Container();
+      },
+    );
   }
-
 }
