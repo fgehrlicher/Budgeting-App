a2d694ec3ba72179e20aec2f3cf6731d10f3b4a0
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-11-15 11:35:55 +0100

    Make the account balance model database ready

--- lib/domain/models/account_balance.dart
+++ lib/domain/models/account_balance.dart
@@ -1,12 +1,7 @@
 class AccountBalance {
-  final int _balance;
+  int _balance;
 
   AccountBalance(this._balance);
-
-  @override
-  String toString() {
-    return this._balance.toString() + " €";
-  }
 
   int get balance => _balance;
 }
--- lib/domain/models/balance_snapshot.dart
+++ lib/domain/models/balance_snapshot.dart
@@ -1,14 +1,29 @@
 import 'package:hunger_preventer/domain/models/account_balance.dart';
 
 class AccountBalanceSnapshot extends AccountBalance {
-  final DateTime _date;
+  DateTime _date;
+  int _id;
 
-  AccountBalanceSnapshot(int balance, this._date) : super(balance);
+  AccountBalanceSnapshot(int balance, this._date, [this._id]) : super(balance);
+
+  AccountBalanceSnapshot.fromMap(Map<String, dynamic> data)
+      : super(data['balance']) {
+    this._id = data['id'];
+    this._date = DateTime.fromMillisecondsSinceEpoch(data['date']);
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = {
+      'id': this.id,
+      'date': this.date.millisecondsSinceEpoch,
+      'balance': this.balance
+    };
+
+    map.removeWhere((key, value) => value == null);
+    return map;
+  }
 
   DateTime get date => _date;
 
-  @override
-  String toString() {
-    return '${this.date.day}/${this.date.month}/${this.date.year} ${this.balance} Account Balance Snapshot';
-  }
+  int get id => _id;
 }
