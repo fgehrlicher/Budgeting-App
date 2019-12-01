6631d3426567f2674a4eac91b55888391b127f1a
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-01 16:09:28 +0100

    Rename Account Balance Snapshot to Account Balance

--- lib/domain/models/acount_balance.dart
+++ lib/domain/models/acount_balance.dart
@@ -1,16 +1,16 @@
 import 'package:hunger_preventer/data/database/field_config.dart';
 import 'package:hunger_preventer/data/database/persistent_model.dart';
 
-class AccountBalanceSnapshot implements PersistentModel {
+class AccountBalance implements PersistentModel {
   static const String TABLE_NAME = "accountbalancesnapshot";
 
   int id;
   DateTime date;
   int balance;
 
-  AccountBalanceSnapshot({this.id, this.date, this.balance});
+  AccountBalance({this.id, this.date, this.balance});
 
-  AccountBalanceSnapshot.fromMap(Map<String, dynamic> data){
+  AccountBalance.fromMap(Map<String, dynamic> data){
     this.id = data['id'];
     this.balance = data['balance'];
     this.date = DateTime.fromMillisecondsSinceEpoch(data['date']);
