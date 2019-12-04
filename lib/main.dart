9b1e6498c627bdd0d9d71e06b3d44af0c4257227
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-04 20:42:23 +0100

    Add mock data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hunger_preventer/data/database/database_provider.dart';
+import 'package:hunger_preventer/data/repositories/account_balance_repository.dart';
 import 'package:hunger_preventer/data/repositories/transaction_repository.dart';
+import 'package:hunger_preventer/domain/models/acount_balance.dart';
 import 'package:hunger_preventer/domain/models/transaction.dart';
 import 'package:hunger_preventer/domain/models/transaction_list.dart';
 import 'package:hunger_preventer/presentation/app.dart';
@@ -13,25 +15,27 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  var transactions = TransactionRepository(DatabaseProvider.database);
-  transactions.insertMultiple(TransactionList()
-    ..addAll([
-      Transaction(
-        date: DateTime.parse("2019-01-08 10:00:00"), amount: 1000, id: 1
+  TransactionRepository(DatabaseProvider.database).insertMultiple(
+    TransactionList()
+      ..addAll(
+        [
+          Transaction(
+              date: DateTime.parse("2019-10-08 10:00:00"), amount: 1000, id: 1),
+          Transaction(
+              date: DateTime.parse("2019-13-08 10:00:00"), amount: -100, id: 2),
+          Transaction(
+              date: DateTime.parse("2019-20-08 10:00:00"), amount: -200, id: 3),
+          Transaction(
+              date: DateTime.parse("2019-29-08 10:00:00"), amount: -50, id: 4),
+          Transaction(
+              date: DateTime.parse("2019-01-09 10:00:00"), amount: 1000, id: 5),
+        ],
       ),
-      Transaction(
-        date: DateTime.parse("2019-13-08 10:00:00"), amount: -100, id: 2
-      ),
-      Transaction(
-        date: DateTime.parse("2019-20-08 10:00:00"), amount: -200, id: 3
-      ),
-      Transaction(
-        date: DateTime.parse("2019-29-08 10:00:00"), amount: -50, id: 4
-      ),
-      Transaction(
-        date: DateTime.parse("2019-01-09 10:00:00"), amount: 1000, id: 5
-      ),
-    ]));
+  );
+
+  AccountBalanceRepository(DatabaseProvider.database).insertOne(
+    AccountBalance(id: 1, date: DateTime.parse("2019-01-08 10:00:00"), balance: 10),
+  );
 
   runApp(HungerPreventerApp());
 }
