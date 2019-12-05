794fd3d61db3717c93c6e387ee0a2dc69dbb017a
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-05 17:15:25 +0100

    Extend the home screen with state specific rendering

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hunger_preventer/domain/bloc/home/home_bloc.dart';
+import 'package:hunger_preventer/domain/bloc/home/home_state.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,30 +9,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  HomeBloc _homeBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    this._homeBloc = BlocProvider.of<HomeBloc>(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Available Amount:",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber[800]),
-              textAlign: TextAlign.center,
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state is EmptyBalance) {
+          Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Error",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber[800]),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              "1000,50€",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+          );
+        }
+        if (state is CalculatingBalance) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (state is BalanceCalculated) {
+          Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Available Amount:",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber[800]),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    state.accountBalance.balance.toString() + " \$",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
+          );
+        }
+
+        return Container();
+      },
     );
   }
 }
