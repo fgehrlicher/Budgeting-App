699131355260ae0df9dac83f16809bb94870e95c
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-12 13:41:26 +0100

    Refactore the balance container widget

--- lib/presentation/screens/home/balance_container.dart
+++ lib/presentation/screens/home/balance_container.dart
@@ -1,63 +1,40 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 
 class BalanceContainer extends StatelessWidget {
-  final String headline;
-  final String body;
-  final RefreshCallback refreshCallback;
+  static const String HEADLINE = "Available ";
+  final String balance;
 
-  BalanceContainer(
-      {@required this.headline,
-      @required this.body,
-      @required this.refreshCallback});
+  BalanceContainer(this.balance);
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> columnWidgets = List();
-
-    if (headline != null) {
-      columnWidgets.add(
-        Text(
-          headline,
-          style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Color(0xff0089BA)),
-          textAlign: TextAlign.center,
+    List<Widget> columnWidgets = [
+      Text(
+        HEADLINE,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Color(0xff0089BA),
         ),
-      );
-    }
-    if (body != null) {
+      )
+    ];
+
+    if (balance != null) {
       columnWidgets.add(
         Text(
-          body,
+          balance,
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           textAlign: TextAlign.center,
         ),
       );
     }
 
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return RefreshIndicator(
-          onRefresh: refreshCallback,
-          child: SingleChildScrollView(
-            physics: AlwaysScrollableScrollPhysics(),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight,
-                minWidth: constraints.maxWidth,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: columnWidgets,
-              ),
-            ),
-          ),
-        );
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: columnWidgets,
     );
   }
 }
