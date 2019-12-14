b19dac07d23b28f69016f95888aa578e94fca860
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-14 17:47:06 +0100

    Add a refresh indicator to the transaction screen

--- lib/presentation/screens/transactions/transactions.dart
+++ lib/presentation/screens/transactions/transactions.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unnamed_budgeting_app/domain/bloc/transactions/transactions_bloc.dart';
+import 'package:unnamed_budgeting_app/domain/bloc/transactions/transactions_event.dart';
 import 'package:unnamed_budgeting_app/domain/bloc/transactions/transactions_state.dart';
 import 'package:unnamed_budgeting_app/presentation/screens/edit_transaction/edit_transaction.dart';
 
@@ -11,60 +14,77 @@ class Transactions extends StatefulWidget {
 
 class _TransactionsState extends State<Transactions> {
   TransactionsBloc _transactionsBloc;
+  Completer<void> _refreshCompleter;
 
   @override
   void initState() {
     super.initState();
     this._transactionsBloc = BlocProvider.of<TransactionsBloc>(context);
+    _refreshCompleter = Completer<void>();
   }
 
-  @override
+  RefreshCallback _getRefreshCallback() {
+    return () {
+      _transactionsBloc.add(FetchTransactions());
+      return _refreshCompleter.future;
+    };
+  }
+
   Widget build(BuildContext context) {
-    return BlocBuilder<TransactionsBloc, TransactionsState>(
-      builder: (context, state) {
-        if (state is TransactionsEmpty) {
-          return Center(
-            child: Text('no transactions'),
-          );
-        }
-        if (state is TransactionsLoading) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
+    return BlocListener<TransactionsBloc, TransactionsState>(
+      listener: (context, state) {
         if (state is TransactionsLoaded) {
-          var childen = List<Widget>();
-          state.transactions.forEach((element) {
-            childen.add(
-              Card(
-                child: ListTile(
-                  title: Text("Transaction"),
-                  subtitle: Text(element.amount.toString()),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        fullscreenDialog: true,
-                        builder: (BuildContext context) => EditTransaction(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            );
-          });
-
-          return ListView(
-            padding: const EdgeInsets.all(8),
-            children: childen,
-          );
+          _refreshCompleter?.complete();
+          _refreshCompleter = Completer();
         }
-
-        return Center(
-          child: Text('no transactions'),
-        );
       },
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return BlocBuilder<TransactionsBloc, TransactionsState>(
+            builder: (context, state) {
+              if (state is TransactionsEmpty) {
+                return Center(
+                  child: Text('no transactions'),
+                );
+              }
+
+              if (state is TransactionsLoaded) {
+                var childen = List<Widget>();
+                state.transactions.forEach((element) {
+                  childen.add(
+                    Card(
+                      child: ListTile(
+                        title: Text("Transaction"),
+                        subtitle: Text(element.amount.toString()),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              fullscreenDialog: true,
+                              builder: (BuildContext context) =>
+                                  EditTransaction(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                });
+
+                return RefreshIndicator(
+                  onRefresh: _getRefreshCallback(),
+                  child: ListView(
+                    padding: const EdgeInsets.all(20),
+                    children: childen,
+                  ),
+                );
+              }
+
+              return Container();
+            },
+          );
+        },
+      ),
     );
   }
 }
