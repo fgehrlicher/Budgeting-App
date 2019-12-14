0c98a86e573be9425dcac8a54e75b7966a270d07
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2019-12-14 17:22:18 +0100

    Add single transaction events

--- lib/domain/bloc/transactions/transactions_event.dart
+++ lib/domain/bloc/transactions/transactions_event.dart
@@ -1,10 +1,23 @@
-import 'package:equatable/equatable.dart';
+import 'package:unnamed_budgeting_app/domain/models/transaction.dart';
 
-abstract class TransactionsEvent extends Equatable {
-  const TransactionsEvent();
-
-  @override
-  List<Object> get props => List();
-}
+abstract class TransactionsEvent {}
 
 class FetchTransactions extends TransactionsEvent {}
+
+class AddTransaction extends TransactionsEvent {
+  final Transaction transaction;
+
+  AddTransaction(this.transaction);
+}
+
+class UpdateTransaction extends TransactionsEvent {
+  final Transaction transaction;
+
+  UpdateTransaction(this.transaction);
+}
+
+class DeleteTransaction extends TransactionsEvent {
+  final Transaction transaction;
+
+  DeleteTransaction(this.transaction);
+}
