7862a714f1c65fc9127acab6ef588f7c12a011ef
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-12-28 13:51:24 +0100

    Add image to the salary category

--- lib/domain/models/transaction_categories.dart
+++ lib/domain/models/transaction_categories.dart
@@ -2,10 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:unnamed_budgeting_app/domain/models/transaction_category.dart';
 
 const List<TransactionCategory> transactionCategories = [
-  TransactionCategory(1, "Salary", Icons.attach_money),
-  TransactionCategory(2, "Food & Groceries", Icons.fastfood),
-  TransactionCategory(3, "Shopping", Icons.local_grocery_store),
-  TransactionCategory(4, "Cash withdrawal", Icons.account_balance),
-  TransactionCategory(5, "Transport & car", Icons.directions_bus),
-  TransactionCategory(6, "Online Services", Icons.devices),
+  TransactionCategory(
+    1,
+    "Salary",
+    Icons.attach_money,
+    const AssetImage("assets/images/transaction_salary.png"),
+  ),
+  TransactionCategory(
+    2,
+    "Food & Groceries",
+    Icons.fastfood,
+    const AssetImage(""),
+  ),
+  TransactionCategory(
+    3,
+    "Shopping",
+    Icons.local_grocery_store,
+    const AssetImage(""),
+  ),
+  TransactionCategory(
+    4,
+    "Cash withdrawal",
+    Icons.account_balance,
+    const AssetImage(""),
+  ),
+  TransactionCategory(
+    5,
+    "Transport & car",
+    Icons.directions_bus,
+    const AssetImage(""),
+  ),
+  TransactionCategory(
+    6,
+    "Online Services",
+    Icons.devices,
+    const AssetImage(""),
+  ),
 ];
