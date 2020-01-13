42be2082723f08d0920f70e7a5cbbcc05202cc0b
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-13 08:29:21 +0100

    Remove the equalable interface

--- lib/domain/bloc/transactions/transactions_state.dart
+++ lib/domain/bloc/transactions/transactions_state.dart
@@ -1,30 +1,23 @@
-import 'package:equatable/equatable.dart';
 import 'package:unnamed_budgeting_app/domain/models/transaction.dart';
 import 'package:unnamed_budgeting_app/domain/models/transaction_list.dart';
 
-abstract class TransactionsState extends Equatable {
+abstract class TransactionsState {
   const TransactionsState();
-
-  @override
-  List<Object> get props => List();
 }
 
-class TransactionsEmpty extends TransactionsState{}
+class TransactionsEmpty extends TransactionsState {}
 
-class TransactionsInitialLoading extends TransactionsState{}
+class TransactionsInitialLoading extends TransactionsState {}
 
-class TransactionsLoading extends TransactionsState{}
+class TransactionsLoading extends TransactionsState {}
 
-class TransactionsLoaded extends TransactionsState{
+class TransactionsLoaded extends TransactionsState {
   final TransactionList transactions;
 
   const TransactionsLoaded(this.transactions);
-
-  @override
-  List<Object> get props => transactions;
 }
 
-class TransactionsDeleted extends TransactionsState{
+class TransactionsDeleted extends TransactionsState {
   final Transaction transaction;
 
   const TransactionsDeleted(this.transaction);
