55834d3bb37a770605cbfd455b42242032d8e45e
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-20 17:58:11 +0100

    Rework dummy data generation

---
+++ lib/dummy_data.dart
@@ -0,0 +1,50 @@
+import 'package:faker/faker.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:unnamed_budgeting_app/data/database/database_provider.dart';
+import 'package:unnamed_budgeting_app/data/repositories/account_balance_repository.dart';
+import 'package:unnamed_budgeting_app/data/repositories/transaction_repository.dart';
+import 'package:unnamed_budgeting_app/domain/models/acount_balance.dart';
+import 'package:unnamed_budgeting_app/domain/models/transaction.dart' as model;
+import 'package:unnamed_budgeting_app/domain/models/transaction_categories.dart';
+import 'package:unnamed_budgeting_app/domain/models/transaction_list.dart';
+import 'dart:math';
+
+class DummyData {
+  void insertDummyData() {
+    var database = DatabaseProvider.database;
+    var random = Random();
+    var transactionRepository = TransactionRepository(database);
+    var accountBalanceRepository = AccountBalanceRepository(database);
+    var dummyTransactions = List<model.Transaction>.generate(
+      200,
+      (int index) => model.Transaction(
+        title: faker.lorem.words(3).join(" "),
+        category:
+            transactionCategories[random.nextInt(transactionCategories.length)],
+        date: DateTime.parse(
+            "2019-12-${random.nextInt(20) + 10} ${random.nextInt(13) + 10}:${random.nextInt(49) + 10}:${random.nextInt(49) + 10}"),
+        amount: random.nextInt(600000) - 300000,
+      ),
+    );
+
+    database.then((Database database) {
+      var tableName = model.Transaction.TABLE_NAME;
+      database.rawQuery("Delete from $tableName;");
+      database.rawQuery(
+        "DELETE FROM SQLITE_SEQUENCE WHERE name='$tableName';",
+      );
+    });
+
+    transactionRepository.insertMultiple(
+      TransactionList()..addAll(dummyTransactions),
+    );
+
+    accountBalanceRepository.insert(
+      AccountBalance(
+        id: 1,
+        date: DateTime.parse("2019-28-11 10:00:00"),
+        balance: 23200,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:unnamed_budgeting_app/data/database/database_provider.dart';
-import 'package:unnamed_budgeting_app/data/repositories/account_balance_repository.dart';
-import 'package:unnamed_budgeting_app/data/repositories/transaction_repository.dart';
-import 'package:unnamed_budgeting_app/domain/models/acount_balance.dart';
-import 'package:unnamed_budgeting_app/domain/models/transaction.dart';
-import 'package:unnamed_budgeting_app/domain/models/transaction_categories.dart';
-import 'package:unnamed_budgeting_app/domain/models/transaction_list.dart';
+import 'package:unnamed_budgeting_app/dummy_data.dart';
 import 'package:unnamed_budgeting_app/presentation/app.dart';
 
 void main() {
@@ -16,77 +10,7 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  TransactionRepository(DatabaseProvider.database).insertMultiple(
-    TransactionList()
-      ..addAll(
-        [
-          Transaction(
-            title: "Salary payment",
-            category: transactionCategories[0],
-            date: DateTime.parse("2019-12-01 10:00:00"),
-            amount: 280000,
-            id: 1,
-          ),
-          Transaction(
-            title: "Rent",
-            date: DateTime.parse("2019-12-02 09:00:00"),
-            amount: -120000,
-            id: 2,
-          ),
-          Transaction(
-            title: "Cash withdrawal",
-            category: transactionCategories[3],
-            date: DateTime.parse("2019-12-05 15:00:00"),
-            amount: -20000,
-            id: 3,
-          ),
-          Transaction(
-            title: "Netflix",
-            category: transactionCategories[5],
-            date: DateTime.parse("2019-12-12 15:00:00"),
-            amount: -1000,
-            id: 4,
-          ),
-          Transaction(
-            title: "Spotify",
-            category: transactionCategories[5],
-            date: DateTime.parse("2019-12-12 23:00:00"),
-            amount: -1000,
-            id: 5,
-          ),
-          Transaction(
-            title: "Bar",
-            date: DateTime.parse("2019-12-13 02:32:00"),
-            amount: -7000,
-            id: 6,
-          ),
-          Transaction(
-            title: "Grocery Shopping",
-            category: transactionCategories[1],
-            date: DateTime.parse("2019-12-20 12:00:00"),
-            amount: -15000,
-            id: 7,
-          ),
-          Transaction(
-            title: "Water and Electricity",
-            date: DateTime.parse("2019-12-23 12:00:00"),
-            amount: -20000,
-            id: 8,
-          ),
-          Transaction(
-            title: "Loan",
-            date: DateTime.parse("2019-12-23 12:00:00"),
-            amount: -12310,
-            id: 9,
-          ),
-        ],
-      ),
-  );
-
-  AccountBalanceRepository(DatabaseProvider.database).insert(
-    AccountBalance(
-        id: 1, date: DateTime.parse("2019-28-11 10:00:00"), balance: 23200),
-  );
+  DummyData()..insertDummyData();
 
   runApp(UnnamedBudgetingApp());
 }
