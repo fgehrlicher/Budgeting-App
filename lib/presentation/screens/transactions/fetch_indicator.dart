e19ffc0b46587329093b0319bf8353291c4ba83c
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-21 09:04:07 +0100

    Add the fetch indicator widgte

---
+++ lib/presentation/screens/transactions/fetch_indicator.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+enum FetchIndicatorMode {
+  Sleeping,
+  Fetching,
+  NoTransactionsLeft,
+}
+
+class FetchIndicator extends StatefulWidget {
+  final _FetchIndicatorState _fetchIndicatorState = _FetchIndicatorState();
+
+  void setSleeping() {
+    _fetchIndicatorState.setMode(FetchIndicatorMode.Sleeping);
+  }
+
+  void setFetching() {
+    _fetchIndicatorState.setMode(FetchIndicatorMode.Fetching);
+  }
+
+  void setNoTransactionsLeft() {
+    _fetchIndicatorState.setMode(FetchIndicatorMode.NoTransactionsLeft);
+  }
+
+  @override
+  _FetchIndicatorState createState() => _fetchIndicatorState;
+}
+
+class _FetchIndicatorState extends State<FetchIndicator> {
+  FetchIndicatorMode _fetchIndicatorMode;
+
+  void setMode(FetchIndicatorMode _mode) {
+    setState(() {
+      _fetchIndicatorMode = _mode;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchIndicatorMode = FetchIndicatorMode.Sleeping;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch(_fetchIndicatorMode) {
+      case FetchIndicatorMode.Sleeping: {
+        return Container();
+      }
+      break;
+
+      case FetchIndicatorMode.Fetching: {
+        return CircularProgressIndicator();
+      }
+      break;
+
+      case FetchIndicatorMode.NoTransactionsLeft: {
+        return Text('no more transactions left');
+      }
+      break;
+
+      default: {
+        return Container();
+      }
+      break;
+    }
+  }
