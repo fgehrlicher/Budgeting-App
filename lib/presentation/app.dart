ce334eb770aeaeb74bc73757c2afb4105f20a020
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-25 23:26:23 +0100

    Add dynamic theme

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,18 +1,17 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:unnamed_budgeting_app/presentation/frame.dart';
 
 class UnnamedBudgetingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Frame(),
-      theme: ThemeData(
-        brightness: Brightness.light,
+    return DynamicTheme(
+      defaultBrightness: Brightness.dark,
+      data: (brightness) => ThemeData(brightness: brightness),
+      themedWidgetBuilder: (context, theme) => MaterialApp(
+        home: Frame(),
+        theme: theme,
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      themeMode: ThemeMode.dark,
     );
   }
 }
