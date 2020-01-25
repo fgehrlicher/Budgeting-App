ee40d1559d3774f0bcc4984cf534c47940e30e50
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-25 23:26:47 +0100

    Implemenet a dark mode toggle

--- lib/presentation/screens/settings/settings.dart
+++ lib/presentation/screens/settings/settings.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:unnamed_budgeting_app/presentation/screens/settings/settings_toggle.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -6,7 +8,27 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  void changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+        Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark);
+  }
 
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          SettingsToggle(
+            title: 'Dark Theme',
+            onChanged: (_) => changeBrightness(),
+            value: Theme.of(context).brightness == Brightness.dark,
+          ),
+        ],
+      ),
+    );
   }
 }
