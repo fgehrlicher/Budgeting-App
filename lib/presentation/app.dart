77ace10477ff674a7b1c1c9f13649322486d0b2c
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-25 18:25:08 +0100

    Remove old import

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:unnamed_budgeting_app/presentation/frame.dart';
-import 'package:unnamed_budgeting_app/presentation/styles.dart';
 
 class UnnamedBudgetingApp extends StatelessWidget {
   @override
