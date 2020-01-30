1795e5bd18429443573919e01c3ffec839397917
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-30 08:29:57 +0100

    Make the transaction category a persistent model

--- lib/domain/models/transaction_category.dart
+++ lib/domain/models/transaction_category.dart
@@ -1,53 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:unnamed_budgeting_app/data/database/field_config.dart';
+import 'package:unnamed_budgeting_app/data/database/persistent_model.dart';
+import 'package:unnamed_budgeting_app/data/database/sqlite_types.dart';
 
-class TransactionCategory {
-  final int id;
-  final String title;
-  final IconData iconData;
-  final Color backgroundColor;
+class TransactionCategory implements PersistentModel {
+  static const String TABLE_NAME = "usertransactioncategory";
 
-  const TransactionCategory({this.id, this.title, this.iconData, this.backgroundColor});
+  int id;
+  static const String ID_NAME = "id";
+  static const String ID_CONFIG = SqliteTypes.PRIMARY_KEY;
+
+  String title;
+  static const String TITLE_NAME = "title";
+  static const String TITLE_CONFIG = SqliteTypes.TEXT;
+
+  IconData iconData;
+  static const String ICON_DATA_NAME = "icondata";
+  static const String ICON_DATA_CONFIG = SqliteTypes.INT;
+
+  Color color;
+  static const String COLOR_NAME = "color";
+  static const String COLOR_CONFIG = SqliteTypes.TEXT;
+
+  TransactionCategory({this.id, this.title, this.iconData, this.color});
 
   factory TransactionCategory.fromId(int id) {
     return initialTransactionCategories.firstWhere((element) => element.id == id);
   }
+
+  @override
+  String get tableName {
+    return TABLE_NAME;
+  }
+
+  @override
+  List<FieldConfig> get fieldConf {
+    return [
+      FieldConfig(ID_NAME, ID_CONFIG),
+      FieldConfig(TITLE_NAME, TITLE_CONFIG),
+      FieldConfig(ICON_DATA_NAME, ICON_DATA_CONFIG),
+      FieldConfig(COLOR_NAME, COLOR_CONFIG),
+    ];
+  }
+
+  TransactionCategory.fromMap(Map<String, dynamic> data) {
+    id = data[ID_NAME];
+    title = data[TITLE_NAME];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = {
+      ID_NAME: id,
+      TITLE_NAME: title,
+    };
+
+    map.removeWhere((key, value) => value == null);
+    return map;
+  }
+
 }
 
-const List<TransactionCategory> initialTransactionCategories = [
+List<TransactionCategory> initialTransactionCategories = [
   TransactionCategory(
     id: 1,
     title: "Salary",
     iconData: Icons.attach_money,
-    backgroundColor: Colors.orangeAccent,
+    color: Colors.orangeAccent,
   ),
   TransactionCategory(
     id: 2,
     title: "Food & Groceries",
     iconData: Icons.fastfood,
-    backgroundColor: Colors.tealAccent,
+    color: Colors.tealAccent,
   ),
   TransactionCategory(
     id: 3,
     title: "Shopping",
     iconData: Icons.local_grocery_store,
-    backgroundColor: Colors.purpleAccent,
+    color: Colors.purpleAccent,
   ),
   TransactionCategory(
     id: 4,
     title: "Cash withdrawal",
     iconData: Icons.account_balance,
-    backgroundColor: Colors.amberAccent,
+    color: Colors.amberAccent,
   ),
   TransactionCategory(
     id: 5,
     title: "Transport & car",
     iconData: Icons.directions_bus,
-    backgroundColor: Colors.blueAccent,
+    color: Colors.blueAccent,
   ),
   TransactionCategory(
     id: 6,
     title: "Online Services",
     iconData: Icons.devices,
-    backgroundColor: Colors.deepOrange,
+    color: Colors.deepOrange,
   ),
 ];
