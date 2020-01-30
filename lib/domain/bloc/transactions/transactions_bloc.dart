22a75beda85033c8fe2cfec9d35f26d3d24658d3
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-01-30 11:27:05 +0100

    Extend the transactions bloc with transaction category retrieval

--- lib/domain/bloc/transactions/transactions_bloc.dart
+++ lib/domain/bloc/transactions/transactions_bloc.dart
@@ -1,15 +1,21 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:unnamed_budgeting_app/data/repositories/transaction_category_repository.dart';
 import 'package:unnamed_budgeting_app/data/repositories/transaction_repository.dart';
 import 'package:unnamed_budgeting_app/domain/bloc/transactions/transactions_event.dart';
 import 'package:unnamed_budgeting_app/domain/bloc/transactions/transactions_state.dart';
 import 'package:unnamed_budgeting_app/domain/models/transaction.dart';
+import 'package:unnamed_budgeting_app/domain/models/transaction_category.dart';
 import 'package:unnamed_budgeting_app/domain/models/transaction_list.dart';
 
 class TransactionsBloc extends Bloc<TransactionsEvent, TransactionsState> {
   final TransactionRepository _transactionRepository;
+  final TransactionCategoryRepository _transactionCategoryRepository;
 
-  TransactionsBloc(this._transactionRepository);
+  TransactionsBloc(
+    this._transactionRepository,
+    this._transactionCategoryRepository,
+  );
 
   @override
   TransactionsState get initialState => TransactionsInitialLoading();
@@ -38,6 +44,12 @@ class TransactionsBloc extends Bloc<TransactionsEvent, TransactionsState> {
   Stream<TransactionsState> _mapLoadTransactionsToState() async* {
     var transactions = await _transactionRepository.getAll();
     transactions.sortBy(TransactionListSorting.DateDescending);
+    transactions.map((transaction) async {
+      var transactionCategory = await _transactionCategoryRepository
+          .get(transaction.id);
+
+      transaction.category = transactionCategory;
+    });
 
     if (transactions.length > 0) {
       yield TransactionsLoaded(transactions);
@@ -46,17 +58,23 @@ class TransactionsBloc extends Bloc<TransactionsEvent, TransactionsState> {
     }
   }
 
-  Stream<TransactionsState> _mapDeleteTransactionToState(Transaction transaction) async* {
+  Stream<TransactionsState> _mapDeleteTransactionToState(
+    Transaction transaction,
+  ) async* {
     //_transactionRepository.delete(transaction);
     yield TransactionDeleted(transaction);
   }
 
-  Stream<TransactionsState> _mapAddTransactionToState(Transaction transaction) async* {
+  Stream<TransactionsState> _mapAddTransactionToState(
+    Transaction transaction,
+  ) async* {
     //_transactionRepository.add(transaction);
     yield TransactionAdded(transaction);
   }
 
-  Stream<TransactionsState> _mapRestoreTransactionToState(Transaction transaction) async* {
+  Stream<TransactionsState> _mapRestoreTransactionToState(
+    Transaction transaction,
+  ) async* {
     //_transactionRepository.add(transaction);
     yield TransactionRestored(transaction);
   }
