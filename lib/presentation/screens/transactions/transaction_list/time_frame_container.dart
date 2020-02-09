21a77ca61fac1fd6a87a275dbd9ed5c9e58db631
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-02-09 19:41:36 +0100

    Add time frame container widget

---
+++ lib/presentation/screens/transactions/transaction_list/time_frame_container.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:unnamed_budgeting_app/domain/model/time_frame.dart';
+import 'package:unnamed_budgeting_app/domain/model/transaction.dart';
+import 'package:unnamed_budgeting_app/presentation/screens/transactions/edit_transaction/edit_transaction.dart';
+import 'package:unnamed_budgeting_app/presentation/screens/transactions/transaction_list/card_item.dart';
+
+class TimeFrameContainer extends StatefulWidget {
+  final TimeFrame _timeFrame;
+
+  TimeFrameContainer(this._timeFrame);
+
+  @override
+  State createState() => _TimeFrameContainerState(_timeFrame);
+}
+
+class _TimeFrameContainerState extends State<TimeFrameContainer> {
+  final TimeFrame _timeFrame;
+
+  _TimeFrameContainerState(this._timeFrame);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: _timeFrame.transactions.map<Widget>((Transaction transaction) {
+        return CardItem(
+          transaction,
+          () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                fullscreenDialog: true,
+                builder: (BuildContext context) => EditTransaction(transaction),
+              ),
+            );
+          },
+        );
+      }),
+    );
+  }
+}
