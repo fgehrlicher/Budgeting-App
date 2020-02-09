aec34972a0eba2f19371dafc54bf365f0a6accde
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-02-09 19:40:39 +0100

    Refactore card item

--- lib/presentation/screens/transactions/transaction_list/card_item.dart
+++ lib/presentation/screens/transactions/transaction_list/card_item.dart
@@ -4,69 +4,59 @@ import 'package:unnamed_budgeting_app/domain/model/transaction.dart';
 class CardItem extends StatelessWidget {
   final Transaction _transaction;
   final VoidCallback _onTap;
-  final Animation<double> _animation;
-  final Color _color;
 
   const CardItem(
     this._transaction,
     this._onTap,
-    this._animation,
-    this._color,
   );
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
-      child: SizeTransition(
-        axis: Axis.vertical,
-        sizeFactor: _animation,
-        child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: _onTap,
-          child: Card(
-            color: _color,
-            child: ListTile(
-              leading: Container(
-                color: _transaction.category?.color ?? Colors.grey,
-                width: 50,
-                height: 50,
-                child: _transaction.category != null
-                    ? Icon(
-                        _transaction.category?.icon?.iconData,
-                        size: 30,
-                      )
-                    : Container(
-                        height: 30,
-                        width: 30,
-                      ),
-              ),
-              title: Text(
-                _transaction.title,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              isThreeLine: true,
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    _transaction.formattedBalance,
-                    style: TextStyle(
-                      color:
-                          _transaction.amount > 0 ? Colors.green : Colors.red,
-                      fontSize: 15,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: _onTap,
+        child: Card(
+          child: ListTile(
+            leading: Container(
+              color: _transaction.category?.color ?? Colors.grey,
+              width: 50,
+              height: 50,
+              child: _transaction.category != null
+                  ? Icon(
+                      _transaction.category?.icon?.iconData,
+                      size: 30,
+                    )
+                  : Container(
+                      height: 30,
+                      width: 30,
                     ),
-                  ),
-                  Text(
-                    _transaction.formattedDate,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ],
-              ),
-              onTap: _onTap,
             ),
+            title: Text(
+              _transaction.title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            isThreeLine: true,
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _transaction.formattedBalance,
+                  style: TextStyle(
+                    color: _transaction.amount > 0 ? Colors.green : Colors.red,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  _transaction.formattedDate,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            onTap: _onTap,
           ),
         ),
       ),
